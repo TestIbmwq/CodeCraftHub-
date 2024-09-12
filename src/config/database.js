49da933c402748cd1e1d8b85f20bd78798358566
TestIbmwq <quinnwd@yahoo.com>
2024-09-12 16:54:59 -0400

    Adding temporary changes to Github

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost/user_database', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
+    await mongoose.connect('mongodb://localhost:27017/user_database', {
+      //useNewUrlParser: true,
+      //useUnifiedTopology: true,
     });
     console.log('MongoDB connected');
   } catch (error) {
